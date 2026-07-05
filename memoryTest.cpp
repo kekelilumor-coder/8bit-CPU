a89910aa9a2efe078d7eb6e26d68442966c94c07
kekeli <kekelilumor@gmail.com>
2026-07-05 22:40:22 +0100

    memory Test

---
+++ memoryTest.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdint>
+#include <iomanip>
+
+int main(){
+    uint8_t array[256];
+    
+    //setw(2): sets length of all hex values to 2
+    //setfill('0'): uses 0 as filler character for single digit hex values
+    for (int i = 0; i < 256; i++){
+        array[i] = i;
+        std::cout << "0x" << std::hex << std::setw(2) << std::setfill('0') << (int)array[i] << "\n";
+    }
+}
